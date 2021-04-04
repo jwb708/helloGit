3c125f9fc3c6a9abd4d9c3e80604746c272f1e55
jwb <b8y8z8@126.com>
2021-04-04 09:19:59 +0800

    第二次提交

--- HelloGit/src/main/java/com/bytedance/git/test/Demo.java
+++ HelloGit/src/main/java/com/bytedance/git/test/Demo.java
@@ -3,5 +3,6 @@ package com.bytedance.git.test;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Git !!!");
+        System.out.println("更新2");
     }
 }
